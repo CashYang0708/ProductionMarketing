c89632bf6a4299046a3e00eefa804fa7ec0a2150
didiwannasleep0117 <78998069+didiwannasleep0117@users.noreply.github.com>
2022-01-02 23:10:15 +0800

    '???'

--- bun/wsgi.py
+++ bun/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling # <- 加入
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bun.settings")
 application = Cling(get_wsgi_application()) # <- 修改
