2f21bca51bcd04769b18fc1e5c7b9ce34018832f
didiwannasleep0117 <78998069+didiwannasleep0117@users.noreply.github.com>
2022-01-03 00:08:18 +0800

    '?'

--- bun/production_settings.py
+++ bun/production_settings.py
@@ -4,7 +4,12 @@ from .settings import * # 含入原本的settings.py所有設定
 DATABASES = {
     'default': dj_database_url.config(),
 }
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # 設定HTTP連線方式
 ALLOWED_HOSTS = ['*'] # 讓所有的網域都能瀏覽本網站
 DEBUG = False # 關閉除錯模式
