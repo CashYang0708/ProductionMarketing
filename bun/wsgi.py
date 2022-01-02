9e25923da3036e87f397a121a00f5a36198bb585
didiwannasleep0117 <78998069+didiwannasleep0117@users.noreply.github.com>
2022-01-02 23:21:51 +0800

    ':('

--- bun/wsgi.py
+++ bun/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling # <- 加入
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-if path not in sys.path:
-    sys.path.append(path)
-    
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bun.settings")
 application = Cling(get_wsgi_application()) # <- 修改
